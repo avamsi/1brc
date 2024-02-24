04ef336ac07b27b6eb225d2acc9f373bfaf34e11
Vamsi Avula <avamsi07@gmail.com>
2024-02-24 21:07:32 +0530

    switch to mmap proper and process concurrently
    go run . > /dev/null  15.58s user 0.75s system 826% cpu 1.975 total

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module github.com/avamsi/1brc
 
 go 1.22.0
 
-require (
-	github.com/avamsi/ergo v0.0.0-20240224095625-16fbb814736a
-	golang.org/x/exp v0.0.0-20240222234643-814bf88cf225
-)
+require github.com/avamsi/ergo v0.0.0-20240224095625-16fbb814736a
--- main.go
+++ main.go
@@ -1,50 +1,23 @@
 package main
 
 import (
-	"bufio"
+	"bytes"
 	"fmt"
-	"io"
 	"os"
 	"runtime"
 	"runtime/pprof"
 	"runtime/trace"
-	"strings"
 	"sync"
+	"syscall"
 
 	"github.com/avamsi/ergo/assert"
-	"golang.org/x/exp/mmap"
 )
-
-func readInBatches() <-chan []string {
-	const (
-		chanBufferSize = 1_000
-		batchSize      = 1_000
-	)
-	c := make(chan []string, chanBufferSize)
-	go func() {
-		defer close(c)
-		var (
-			f = assert.Ok(mmap.Open("resources/measurements_10_8.txt"))
-			s = bufio.NewScanner(io.NewSectionReader(f, 0, int64(f.Len())))
-		)
-		lines := make([]string, 0, batchSize)
-		for s.Scan() {
-			lines = append(lines, s.Text())
-			if len(lines) == cap(lines) {
-				c <- lines
-				lines = make([]string, 0, batchSize)
-			}
-		}
-		c <- lines
-	}()
-	return c
-}
 
 type result struct {
 	min, max, sum, count int64
 }
 
-func parseMeasurement(m string) int64 {
+func parseMeasurement(m []byte) int64 {
 	negative := m[0] == '-'
 	if negative {
 		m = m[1:]
@@ -60,7 +33,7 @@ func parseMeasurement(m string) int64 {
 	case 4:
 		p = int64(m[0])*100 + int64(m[1])*10 + int64(m[3]) - z111
 	default:
-		panic(m)
+		panic(string(m))
 	}
 	if negative {
 		return -p
@@ -68,15 +41,25 @@ func parseMeasurement(m string) int64 {
 	return p
 }
 
-func updateResults(lines []string, results map[string]*result) {
-	for _, line := range lines {
-		name, m, ok := strings.Cut(line, ";")
+const numStations = 10_000
+
+func processChunk(chunk []byte) map[string]*result {
+	var (
+		results = make(map[string]*result, numStations)
+		line    []byte
+	)
+	line, chunk, _ = bytes.Cut(chunk, []byte{'\n'})
+	for len(line) > 0 {
+		name, m, ok := bytes.Cut(line, []byte{';'})
 		if !ok {
-			panic(line)
+			panic(string(line))
 		}
-		p := parseMeasurement(m)
-		if r, ok := results[name]; !ok {
-			results[name] = &result{p, p, p, 1}
+		var (
+			s = string(name)
+			p = parseMeasurement(m)
+		)
+		if r, ok := results[s]; !ok {
+			results[s] = &result{p, p, p, 1}
 		} else {
 			if p < r.min {
 				r.min = p
@@ -85,28 +68,9 @@ func updateResults(lines []string, results map[string]*result) {
 			}
 			r.sum, r.count = r.sum+p, r.count+10
 		}
+		line, chunk, _ = bytes.Cut(chunk, []byte{'\n'})
 	}
-}
-
-func reduceResults(results []map[string]*result) *SkipList[string, *result] {
-	var global SkipList[string, *result]
-	for _, shard := range results {
-		for n, r := range shard {
-			gr, ok := global.Get(n)
-			if !ok {
-				global.Put(n, r)
-				continue
-			}
-			if r.min < gr.min {
-				gr.min = r.min
-			}
-			if r.max > gr.max {
-				gr.max = r.max
-			}
-			gr.sum, gr.count = gr.sum+r.sum, gr.count+r.count
-		}
-	}
-	return &global
+	return results
 }
 
 func main() {
@@ -121,24 +85,52 @@ func main() {
 	}()
 
 	var (
-		batches = readInBatches()
-		results = make([]map[string]*result, runtime.NumCPU()-2)
-		g       sync.WaitGroup
+		f          = assert.Ok(os.Open("resources/measurements_10_8.txt"))
+		fileSize   = int(assert.Ok(f.Stat()).Size())
+		data       = assert.Ok(syscall.Mmap(int(f.Fd()), 0, fileSize, syscall.PROT_READ, syscall.MAP_PRIVATE))
+		numWorkers = runtime.NumCPU() - 2
+		chunkSize  = fileSize / numWorkers
+		results    = make(chan map[string]*result, numWorkers+2)
+		g          sync.WaitGroup
 	)
-	const numStations = 10_000
-	for i := range results {
+	defer syscall.Munmap(data)
+
+	for len(data) > chunkSize {
+		var (
+			i     = chunkSize + bytes.IndexByte(data[chunkSize:], '\n')
+			chunk []byte
+		)
+		chunk, data = data[:i], data[i+1:]
 		g.Add(1)
 		go func() {
-			results[i] = make(map[string]*result, numStations)
-			for batch := range batches {
-				updateResults(batch, results[i])
-			}
+			results <- processChunk(chunk)
 			g.Done()
 		}()
 	}
-	g.Wait()
+	go func() {
+		results <- processChunk(data)
+		g.Wait()
+		close(results)
+	}()
 
-	global := reduceResults(results)
+	var global SkipList[string, *result]
+	for chunk := range results {
+		for name, r := range chunk {
+			gr, ok := global.Get(name)
+			if !ok {
+				global.Put(name, r)
+				continue
+			}
+			if r.min < gr.min {
+				gr.min = r.min
+			}
+			if r.max > gr.max {
+				gr.max = r.max
+			}
+			gr.sum, gr.count = gr.sum+r.sum, gr.count+r.count
+		}
+	}
+
 	global.Items()(func(name string, r *result) bool {
 		fmt.Printf("%s=%.1f/%.1f/%.1f\n",
 			name, float64(r.min)/10, float64(r.sum)/float64(r.count), float64(r.max)/10)
